f64a36fbd6b8c04adb2558290c5861dcc01b25f8
Prasad Naik <naikprasad0543@gmail.com>
2019-10-11 02:03:19 +0530

    Created Modal popupp for Education and resume download button

---
+++ Assets/script.js
@@ -0,0 +1,9 @@
+// When the user clicks on the button, open the modal
+function showModalPopup(modal) {
+  document.getElementById(modal).style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function closeModal(modal) {
+  document.getElementById(modal).style.display = "none";
+}
